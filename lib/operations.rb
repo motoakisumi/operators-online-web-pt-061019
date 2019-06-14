4b37ecc8a372cd71fdd7a7eb1411402ce829acf9
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-14 13:11:20 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -4,13 +4,13 @@ def unsafe?(speed)
   if speed < 40 || speed > 60
     return "true"
   else
-    return false
+    return "false"
 end
 
 
 
 def not_safe?(speed)
-  puts true if speed < 40 || speed >60
+  puts "true" if speed < 40 || speed >60
 end
 	
 
